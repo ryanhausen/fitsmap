215ac73e972cf00ba9b9515f1ca73af7a997ca64
ryan hausen <ryan.hausen@gmail.com>
2019-11-20 00:05:06 -0800

    missed a file

--- fitsmap/mapmaker.py
+++ fitsmap/mapmaker.py
@@ -24,8 +24,9 @@ import shutil
 import sys
 from functools import partial, reduce
 from itertools import chain, count, filterfalse, product, repeat
-from multiprocessing import Pool
-from typing import Callable, List, Tuple, Union
+from multiprocessing import JoinableQueue, Pool, Process
+from queue import Empty
+from typing import Callable, Iterable, List, Tuple, Union
 
 import sharedmem
 import matplotlib.pyplot as plt
@@ -33,9 +34,12 @@ import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 from imageio import imread
+
 # https://github.com/zimeon/iiif/issues/11#issuecomment-131129062
 from PIL import Image
 from tqdm import tqdm
+
+from fitsmap.web_map import Map
 
 Image.MAX_IMAGE_PIXELS = sys.maxsize
 
@@ -48,94 +52,7 @@ IMG_ENGINE_PIL = "PIL"
 IMG_ENGINE_MPL = "MPL"
 
 
-def dir_to_map(
-    directory: str,
-    tile_size: Shape = [256, 256],
-    depth: int = None,
-    method: str = METHOD_RECURSIVE,
-    image_engine: str = convert.IMG_ENGINE_MPL,
-    title: str = "FitsMap",
-    multiprocessing_processes: int = 0,
-    cat_wcs_fits_file: str = None,
-    out_dir: str = None,
-):
-    if out_dir:
-        _map = Map(out_dir, title)
-    else:
-        _map = Map(directory, title)
-
-    dir_entries = list(
-        map(
-            lambda d: os.path.join(directory, d),
-            filter(
-                lambda d: os.path.splitext(d)[1][1:] in IMG_FORMATS,
-                os.listdir(directory),
-            ),
-        )
-    )
-
-    kwargs = dict(
-        tile_size=tile_size,
-        depth=depth,
-        method=method,
-        image_engine=image_engine,
-        out_dir=out_dir,
-    )
-    layer_func = partial(img_to_layer, **kwargs)
-
-    pbar_loc_generator = count(0)
-    if multiprocessing_processes > 0:
-        with Pool(multiprocessing_processes) as p:
-            built_layers = p.starmap(layer_func, zip(dir_entries, pbar_loc_generator))
-    else:
-        built_layers = map(layer_func, dir_entries, pbar_loc_generator)
-
-    def update_map(shp1: Tuple[int, int], layer: Tuple[str, str, Shape]) -> np.ndarray:
-        p, n, shp2 = layer
-        _map.add_tile_layer(p, n)
-        return np.maximum(shp1, shp2)
-
-    max_xy = reduce(update_map, built_layers, (0, 0))
-
-    catalogs = list(
-        map(
-            lambda d: os.path.join(directory, d),
-            filter(lambda d: d.endswith(".cat"), os.listdir(directory)),
-        )
-    )
-
-    if len(catalogs) > 0:
-        if cat_wcs_fits_file is None:
-            err_msg = [
-                "There are catalog(.cat) in files in " + directory + ", but no",
-                "value was given for 'cat_wcs_fits_file' skipping catalog coversion",
-            ]
-            print(" ".join(err_msg))
-        else:
-            cat_func = partial(convert.catalog, cat_wcs_fits_file, out_dir, max_xy)
-
-            if multiprocessing_processes > 0:
-                with Pool(multiprocessing_processes) as p:
-                    list(
-                        map(
-                            _map.add_marker_catalog,
-                            list(
-                                p.starmap(cat_func, zip(catalogs, pbar_loc_generator))
-                            ),
-                        )
-                    )
-            else:
-                # unlist this maybe?
-                list(
-                    map(
-                        _map.add_marker_catalog,
-                        list(map(cat_func, catalogs, pbar_loc_generator)),
-                    )
-                )
-
-    _map.build_map()
-
-def build_path(depth, y, x, out_dir):
+def build_path(z, y, x, out_dir):
     z, y, x = str(z), str(y), str(x)
     z_dir = os.path.join(out_dir, z)
     y_dir = os.path.join(z_dir, y)
@@ -145,11 +62,13 @@ def build_path(depth, y, x, out_dir):
     return img_path
 
 
-def slice_idx_generator(shape:Tuple[int, int], zoom:int) -> Iterable[Tuple[int, int, int, slice, slice]]:
+def slice_idx_generator(
+    shape: Tuple[int, int], zoom: int
+) -> Iterable[Tuple[int, int, int, slice, slice]]:
     num_splits = int((4 ** zoom) ** 0.5)
     start, end = 0, shape[0]
-    splits = range(start, end+1, end//num_splits)
-    rows = zip(range(num_splits-1, -1, -1), zip(splits[:-1], splits[1:]))
+    splits = range(start, end + 1, end // num_splits)
+    rows = zip(range(num_splits - 1, -1, -1), zip(splits[:-1], splits[1:]))
     cols = enumerate(zip(splits[:-1], splits[1:]))
     rows_cols = product(rows, cols)
 
@@ -158,6 +77,7 @@ def slice_idx_generator(shape:Tuple[int, int], zoom:int) -> Iterable[Tuple[int, 
         return (zoom, y, x, slice(start_y, end_y), slice(start_x, end_x))
 
     return map(transform_iteration, rows_cols)
+
 
 def get_array(file_location: str) -> np.ndarray:
     _, ext = os.path.splitext(file_location)
@@ -214,12 +134,20 @@ def make_dirs(out_dir, zoom):
     zs = range(zoom + 1)
     list(map(build_zs, zs))
 
-def make_tile(array:np.ndarray, vmin:float, vmax:float, out_dir:str, img_engine:str, job:Tuple[int, int, int, slice, slice]):
+
+def make_tile(
+    array: np.ndarray,
+    vmin: float,
+    vmax: float,
+    out_dir: str,
+    img_engine: str,
+    job: Tuple[int, int, int, slice, slice],
+):
     z, y, x, slice_ys, slice_xs = job
 
     img_path = build_path(z, y, x, out_dir)
 
-    if img_engine==IMG_ENGINE_MPL:
+    if img_engine == IMG_ENGINE_MPL:
         f = plt.figure(dpi=100)
         f.set_size_inches([256 / 100, 256 / 100])
         plt.imshow(
@@ -249,7 +177,7 @@ def tile_img(
     pbar_loc: int,
     tile_size: Shape = [256, 256],
     zoom: int = None,
-    image_engine: str = convert.IMG_ENGINE_PIL,
+    image_engine: str = IMG_ENGINE_PIL,
     out_dir: str = ".",
     mp_procs: int = 0,
 ) -> None:
@@ -268,26 +196,47 @@ def tile_img(
     make_dirs(tile_dir, z)
 
     # tile the image
-    total_tiles = sum([4**i for i in range(z+1)])
-    pbar = tqdm(position=pbar_loc, desc="Converting " + name, unit="tile", total=total_tiles)
+    total_tiles = sum([4 ** i for i in range(z + 1)])
 
-    tile_params = chain.from_iterable([slice_idx_generator(array.shape, i) for i in range(z+1)])
+    tile_params = chain.from_iterable(
+        [slice_idx_generator(array.shape, i) for i in range(z + 1)]
+    )
 
     if mp_procs:
         mp_array = sharedmem.empty_like(array)
         mp_array[...] = array[...]
         work = partial(make_tile, array, arr_min, arr_max, tile_dir, image_engine)
         with Pool(mp_procs) as p:
-            any(p.imap_unordered(work, tqdm(tile_params, desc="tiles", total=total_tiles)))
+            any(
+                p.imap_unordered(
+                    work, tqdm(tile_params, desc="Converting " + name, position=pbar_loc, total=total_tiles, unit="tile")
+                )
+            )
     else:
         work = partial(make_tile, array, arr_min, arr_max, tile_dir, image_engine)
-        any(map(work, tqdm(tile_params, desc="Converting " + name, total=total_tiles, unit="tile")))
+        any(
+            map(
+                work,
+                tqdm(
+                    tile_params,
+                    desc="Converting " + name,
+                    total=total_tiles,
+                    unit="tile",
+                    position=pbar_loc
+                ),
+            )
+        )
 
 
 def get_map_layer_name(file_location: str):
     _, fname = os.path.split(file_location)
     name = os.path.splitext(fname)[0].replace(".", "_").replace("-", "_")
     return name
+
+
+def get_marker_file_names(file_location: str):
+    return os.path.split(file_location)[1] + ".js"
+
 
 def line_to_cols(raw_line: str):
 
@@ -335,7 +284,7 @@ def line_to_json(wcs: WCS, columns: List[str], max_dim: int, src_line: str):
 
 def catalog_to_markers(
     wcs_file: str, out_dir: str, catalog_file: str, pbar_loc: int,
-):
+) -> None:
     wcs = WCS(wcs_file)
 
     f = open(catalog_file, "r")
@@ -379,22 +328,40 @@ def catalog_to_markers(
         j.write(";")
     f.close()
 
-    return cat_file
+
+def async_worker(q: JoinableQueue):
+    BLOCK = True
+    TIMEOUT = 5
+
+    while True:
+        try:
+            f, args = q.get(BLOCK, TIMEOUT)
+            f(*args)
+            q.task_done()
+        except Empty:
+            break
+
 
 def files_to_map(
     files: List[str],
     out_dir: str = ".",
-    # exclude_predicate:Callable = None, (put this in dir to map)
     zoom: int = None,
     title: str = "FitsMap",
-    job_procs: int = 0,
-    procs_per_job: int = 0,
+    task_procs: int = 0,
+    procs_per_task: int = 0,
     cat_wcs_fits_file: str = None,
     tile_size: Shape = [256, 256],
-    image_engine: str = convert.IMG_ENGINE_PIL,
+    image_engine: str = IMG_ENGINE_PIL,
 ):
+    if len(files)==0:
+        raise ValueError("No files provided `files` is an empty list")
+
     img_f_kwargs = dict(
-        tile_size=tile_size, zoom=zoom, image_engine=image_engine, out_dir=out_dir,
+        tile_size=tile_size,
+        zoom=zoom,
+        image_engine=image_engine,
+        out_dir=out_dir,
+        mp_procs=procs_per_task
     )
 
     img_files = filter_on_extension(files, IMG_FORMATS)
@@ -402,24 +369,78 @@ def files_to_map(
     img_job_f = partial(tile_img, **img_f_kwargs)
 
     cat_files = filter_on_extension(files, CAT_FORMAT)
+    marker_file_names = list(map(get_marker_file_names, cat_files))
 
     if len(cat_files) > 0:
         if cat_wcs_fits_file is None:
+            cat_files = marker_file_names = []
             err_msg = [
                 "Catalog files (.cat) were included, but no value was given for"
                 "`cat_wcs_fits_file`. ra/dec can't be converted without a fits"
                 "file. Skipping catalog conversion"
             ]
             print(" ".join(err_msg))
-
-
-        cat_func = partial(catalog_to_markers, cat_wcs_fits_file, out_dir)
-
-        # finish catalogging
-
-    # combine cat and img jobs
-
-    # execute sync or async
-
+        else:
+            cat_job_f = partial(catalog_to_markers, cat_wcs_fits_file, out_dir)
+    else:
+        cat_job_f = None
 
     pbar_locations = count(0)
+
+    img_tasks = zip(repeat(img_job_f), zip(img_files, pbar_locations))
+    cat_tasks = zip(repeat(cat_job_f), zip(cat_files, pbar_locations))
+    tasks = chain(img_tasks, cat_tasks)
+
+    if task_procs:
+        q = JoinableQueue()
+        any(map(lambda t: q.put(t), tasks))
+
+        workers = [Process(target=async_worker, args=[q]) for _ in range(task_procs)]
+        [w.start() for w in workers]  # can use any-map if this returns None
+
+        q.join()
+    else:
+        any(map(lambda func_args: func_args[0](*func_args[1]), tasks))
+
+    print("Building index.html")
+    web_map = Map(out_dir, title)
+    any(map(web_map.add_tile_layer, map_layer_names))
+    any(map(web_map.add_marker_catalog, marker_file_names))
+    web_map.build_map()
+    print("Done")
+
+
+def dir_to_map(
+    directory: str,
+    out_dir: str = ".",
+    exclude_predicate: Callable = lambda f: False,
+    zoom: int = None,
+    title: str = "FitsMap",
+    task_procs: int = 0,
+    procs_per_task: int = 0,
+    cat_wcs_fits_file: str = None,
+    tile_size: Shape = [256, 256],
+    image_engine: str = IMG_ENGINE_PIL,
+):
+    dir_files = list(
+        map(
+            lambda d: os.path.join(directory, d),
+            filterfalse(exclude_predicate, os.listdir(directory),),
+        )
+    )
+
+    if len(dir_files)==0:
+        raise ValueError("No files in `directory` or `exlcude_predicate exlucdes everything")
+
+
+    files_to_map(
+        dir_files,
+        out_dir=out_dir,
+        zoom=zoom,
+        title=title,
+        task_procs=task_procs,
+        procs_per_task=procs_per_task,
+        cat_wcs_fits_file=cat_wcs_fits_file,
+        tile_size=tile_size,
+        image_engine=image_engine,
+    )
