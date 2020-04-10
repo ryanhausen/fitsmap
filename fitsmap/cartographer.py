f6f0f322f42088cacb54d341502e6028b09ddc32
ryan hausen <ryan.hausen@gmail.com>
2020-04-10 14:44:08 -0700

    Fixes #6
    Added CLI
    Updated version to programmatic
    Updated copyright years

--- fitsmap/cartographer.py
+++ fitsmap/cartographer.py
@@ -1,5 +1,5 @@
 # MIT License
-# Copyright 2019 Ryan Hausen
+# Copyright 2020 Ryan Hausen
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
