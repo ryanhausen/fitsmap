614af5c66622957f12cd45e9547ff5d74a87327a
ryanhausen <ryan.hausen@gmail.com>
2021-04-16 00:11:10 -0700

    python 3.7 required now

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Science/Research",
